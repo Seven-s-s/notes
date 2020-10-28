c3778e43b2c1f95b94f931da40765856affb5204
12345678.hu <***>
2020-10-28 11:00:14 +0800

    update index

---
+++ qiyin/js/count.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?08015109ad9e84dcc2d41043cd2a1f5d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
